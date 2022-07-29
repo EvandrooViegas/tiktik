3a784a33c4adb25c2b85d3b4c867210be5e44b08
Valdemiro Viegas <valdemiroviegas@yahoo.com>
2022-07-29 20:51:10 +0100

    fourth

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  
+
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['lh3.googleusercontent.com']
   }
+
+  
 }
 
 module.exports = nextConfig
